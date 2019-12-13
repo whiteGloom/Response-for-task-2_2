d785e6b984bb7be6065b0bb6fef3baeda76f3fd3
whiteGloom <karabin.m@yandex.ru>
2019-12-13 21:29:25 +0700

    Calendar fix #8
    Список изменений:
    • исправление стилей календаря;
    • исправление кнопки today;
    
    #8

--- dev/blocks/calendar/calendar.js
+++ dev/blocks/calendar/calendar.js
@@ -2,12 +2,11 @@ require('jquery-ui/ui/widgets/datepicker');
 
 $('.js-calendar__holder').each((i, el) => {
   $(el).datepicker({
-    date: new Date(),
     firstDay: 1,
+    gotoCurrent: false,
     dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'],
-    showButtonPanel: 'true',
-    showOtherMonths: 'true',
+    showButtonPanel: true,
+    showOtherMonths: true,
     dateFormat: 'dd',
-    gotoCurrent: 'false',
   });
 });
